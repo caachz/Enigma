34f7487848cca8d53b2428b0f151eb8ea0c56d10
Cassie Achzenick <caachzenick@gmail.com>
2020-01-10 09:57:31 -0700

    Add test that enigma exists

---
+++ test/enigma_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def test_it_exists
+    enigma = Enigma.new
+
+    assert_instance_of Engima, enigma
+  end
+end
