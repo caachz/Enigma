4b6d815bc35da389e0b3dfa43d49feee43fb1411
Cassie Achzenick <caachzenick@gmail.com>
2020-01-10 10:42:18 -0700

    Add skips

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,7 +6,6 @@ class EnigmaTest < Minitest::Test
 
   def setup
     @enigma = Enigma.new
-
   end
 
   def test_it_exists
@@ -14,6 +13,7 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_creates_a_randomly_generated_key
+    skip
     enigma = mock("enigma")
     enigma.stubs(:random_number).returns([0,2,7,1,5])
     assert_equal [0,2,7,1,5], enigma.random_number
@@ -22,6 +22,7 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_creates_a_hash_from_date
+    skip
     enigma = mock("enigma")
     enigma.stubs(:date).returns(040895)
 
@@ -35,6 +36,7 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_creates_a_final_shift_hash
+    skip
     enigma = mock("enigma")
     enigma.stubs(:offset).returns({:a => 1, :b => 0, :c => 2, :d => 5})
     enigma.stubs(:key).returns({:a => 02, :b => 27, :c => 71, :d => 15})
@@ -43,13 +45,14 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_shifted_letter_returns_the_correct_shifted_letter
-
+    skip
     assert_equal "k", @enigma.shift("h", 3)
     assert_equal "w", @enigma.shift("d", 73)
     assert_equal "r", @enigma.shift("o", 3)
   end
 
   def test_string_hash_with_letter_as_key_and_shift_amount_as_value
+    skip
     enigma = mock("enigma")
     enigma.stubs(:shift).returns({:a => 3, :b => 27, :c => 73, d: => 20})
 
@@ -57,6 +60,7 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_returns_final_encoded_message
+    skip
     assert_equal "keder ohulw", @enigma.encoded_message({"h" => 3, "e" => 27, "l" => 73, "l" => 20,"o" => 3, " " => 27, "w" => 73, "o" => 20, "r" => 3, "l" => 27, "d" => 73})
   end
 end
